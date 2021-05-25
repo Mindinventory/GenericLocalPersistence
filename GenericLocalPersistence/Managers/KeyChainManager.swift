d5902942df9a4032eaaf9a5055279faf0ae831c2
Riddhi Patel <riddhipatel@Riddhis-Mac-mini.local>
2021-05-25 19:07:45 +0530

    modification in keychaing manager

--- GenericLocalPersistence/Managers/KeyChainManager.swift
+++ GenericLocalPersistence/Managers/KeyChainManager.swift
@@ -8,66 +8,40 @@
 import UIKit
 import Security
 
-// Constant Identifiers
-let userAccount = "AuthenticatedUser"
-let accessGroup = "SecuritySerivice"
-
-
-/**
- *  User defined keys for new entry
- *  Note: add new keys for new secure item and use them in load and save methods
- */
-
-let passwordKey = "KeyForPassword"
-
-// Arguments for the keychain queries
-let kSecClassValue = NSString(format: kSecClass)
-let kSecAttrAccountValue = NSString(format: kSecAttrAccount)
-let kSecValueDataValue = NSString(format: kSecValueData)
-let kSecClassGenericPasswordValue = NSString(format: kSecClassGenericPassword)
-let kSecAttrServiceValue = NSString(format: kSecAttrService)
-let kSecMatchLimitValue = NSString(format: kSecMatchLimit)
-let kSecReturnDataValue = NSString(format: kSecReturnData)
-let kSecMatchLimitOneValue = NSString(format: kSecMatchLimitOne)
-
-
 public class KeyChainManager {
+    
     public  init?() {
     }
-    
-    public  func setPasscode(identifier: String, passcode: String) {
-        if let dataFromString = passcode.data(using: String.Encoding.utf8) {
-            let keychainQuery = [
-                kSecClassValue: kSecClassGenericPasswordValue,
-                kSecAttrServiceValue: identifier,
-                kSecValueDataValue: dataFromString
-            ] as CFDictionary
-            SecItemDelete(keychainQuery)
-            print(SecItemAdd(keychainQuery, nil))
-        }
-        
-        
+    public func save(key: String, data: Data) -> OSStatus {
+        let query = [
+            kSecClass as String       : kSecClassGenericPassword as String,
+            kSecAttrAccount as String : key,
+            kSecAttrService as String:key,
+
+            kSecValueData as String   : data ] as [String : Any]
+
+        SecItemDelete(query as CFDictionary)
+
+        return SecItemAdd(query as CFDictionary, nil)
     }
 
-    public func getPasscode(identifier: String) -> String? {
-        let keychainQuery = [
-            kSecClassValue: kSecClassGenericPasswordValue,
-            kSecAttrServiceValue: identifier,
-            kSecReturnDataValue: kCFBooleanTrue,
-            kSecMatchLimitValue: kSecMatchLimitOneValue
-        ] as  CFDictionary
-        var dataTypeRef: AnyObject?
-        let status: OSStatus = SecItemCopyMatching(keychainQuery, &dataTypeRef)
-        var passcode: String?
-        if (status == errSecSuccess) {
-            if let retrievedData = dataTypeRef as? Data,
-                let result = String(data: retrievedData, encoding: String.Encoding.utf8) {
-                passcode = result as String
-            }
+    public func load(key: String) -> Data? {
+        let query = [
+            kSecClass as String       : kSecClassGenericPassword,
+            kSecAttrAccount as String : key,
+            kSecAttrService as String:key,
+
+            kSecReturnData as String  : kCFBooleanTrue!,
+            kSecMatchLimit as String  : kSecMatchLimitOne ] as [String : Any]
+
+        var dataTypeRef: AnyObject? = nil
+
+        let status: OSStatus = SecItemCopyMatching(query as CFDictionary, &dataTypeRef)
+
+        if status == noErr {
+            return dataTypeRef as! Data?
+        } else {
+            return nil
         }
-        else {
-            print("Nothing was retrieved from the keychain. Status code \(status)")
-        }
-        return passcode
     }
 }
